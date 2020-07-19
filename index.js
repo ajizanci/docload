f7fe7f7f8e3974e2b8676e2721f4e11ff2c92718
Anefu Favour <favouranefu@gmail.com>
2020-07-19 21:33:07 +0100

    Major changes, more reasonable commits moving forward

--- index.js
+++ index.js
@@ -1,58 +1,115 @@
-const axios = require("axios")
-const cheerio = require("cheerio")
-const fs = require("fs")
-const URL = require("url")
-const path = require("path")
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const URL = require("url");
+const path = require("path");
 
 const pathExists = (pathString) => {
-    return new Promise((resolve, reject) => {
-        fs.exists(pathString, (err, exists) => {
-            if (err) reject(err)
-            else resolve(exists)
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    fs.exists(pathString, (err, exists) => {
+      if (err) reject(err);
+      else resolve(exists);
+    });
+  });
+};
 
 async function createPathIfNotExists(pathString) {
-    if (await pathExists(pathString))
-        return
+  if (await pathExists(pathString)) return;
 
-    let pathBuilder = '';
-    for (let i = 0; i < pathString.length; i++) {
-        pathBuilder += pathString[i]
-        if (pathString[i] == '/') {
-            fs.mkdir(pathBuilder, err => err && console.log(err))
-        }
+  let pathBuilder = "";
+  for (let i = 0; i < pathString.length; i++) {
+    pathBuilder += pathString[i];
+    if (pathString[i] == "/" && !(await pathExists(pathString))) {
+      fs.mkdir(pathBuilder, (err) => err && console.log(err));
     }
+  }
 }
 
-async function main() {
-    
-}
-
-const getFileStream = async (url) => (await axios.get(url, { responseType: 'stream' })).data
+const getFileStream = async (url) =>
+  (await axios.get(url, { responseType: "stream" })).data;
 
 const downloadFile = async (url, path) => {
-    const writeStream = fs.createWriteStream(path),
-            readStream = await getFileStream(url);
+  const writeStream = fs.createWriteStream(path),
+    readStream = await getFileStream(url);
 
-    readStream.pipe(writeStream)
-    readStream.on('error', (err) => {
-        throw err
+  readStream.pipe(writeStream);
+  readStream.on("error", (err) => {
+    throw err;
+  });
+};
+
+const getPageName = (urlString) => {
+  if (/\.html$/.test(urlString)) {
+    return urlString;
+  } else if (urlString[urlString.length - 1] == "/") {
+    return urlString + "index.html";
+  } else {
+    return urlString + ".html";
+  }
+};
+
+const stripProtocolFromUrl = (urlString) =>
+  urlString.slice(urlString.indexOf("//") + 2);
+
+function downloadPage(urlString) {
+  createPathIfNotExists(stripProtocolFromUrl(urlString))
+    .then(async () => {
+      const pageName = getPageName(urlString);
+
+      fs.writeFile(
+        pageName,
+        (await axios.get(urlString)).data,
+        (err) => err && console.log(err)
+      );
     })
+    .catch((err) => console.log(err));
 }
 
-const getPageName = pathString => {
-    if (/\.html$/.test(pathString)) {
-        return pathString
-    } else if (pathString[pathString.length - 1] == '/') {
-        return pathString + 'index.html'
-    } else {
-        return pathString + '.html'
-    }
-}
+async function downloadWebsite(urlString) {
+  // const styles = new Map(),
+  //   images = new Map(),
+  const  scripts = new Map(),
+    vistedPages = new Map(),
+    hostname = URL.parse(urlString).hostname;
 
-async function downloadPage(url) {
-    const resolvedUrl = URL.parse(url)
-    createPathIfNotExists(resolvedUrl.host+'/')
+  await createPathIfNotExists(path.resolve('test-site', hostname))
+
+  function crawl(url) {
+    if (vistedPages.has(url)) return;
+
+    vistedPages.set(url, true);
+    const { data } = axios.get(url),
+      $ = cheerio.load(data),
+      pageLinks = $("a"),
+      // pageStyles = $('link[rel=stylesheet]'),
+      pageScripts = $("script");
+    // pageImages = $('img')
+
+    $(pageScripts).each((i, style) => {
+      if (!scripts.has(style.attribs.href)) {
+        const stylePath = path.resolve(
+          "test-site",
+          hostname,
+          "js",
+          style.attribs.src
+        );
+
+        await createPathIfNotExists(stylePath)
+
+        scripts.set(style.attribs.src, stylePath);
+        downloadFile(style, stylePath).then(() => {
+          $(this).attr(
+            "src",
+            path.relative(stripProtocolFromUrl(urlString), stylePath)
+          );
+        });
+      }
+    });
+
+    $(pageLinks).each((i, link) => {
+      const target = link.attribs.href;
+
+      if (URL.parse(target).hostname == hostname) crawl(target);
+    });
+  }
 }
